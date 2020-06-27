e273281e4eddbd7bbf1d27a8ac94f704f3a553ad
00Ryan <00ryan@kakao.com>
2020-06-27 23:47:19 +0900

    입력 받아짐

--- tableControl.js
+++ tableControl.js
@@ -1,20 +1,25 @@
-function tableCreate(){
-    var tableData= document.getElementById('testTable');
-    var row=tableData.insertRow(tableData.rows.length);
+function tableCreate() {
+    var ID = document.getElementById('inName').value;
+    var money = document.getElementById('money').value;
 
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
+    if (money >= 0) {
+        var tableData = document.getElementById('testTable');
+        var row = tableData.insertRow(tableData.rows.length);
 
-    cell1.innerHTML = "<td></td>";
-    cell2.innerHTML = "<td></td>";
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        cell1.innerHTML = "<td>" + ID + "</td>";
+        cell2.innerHTML = "<td>" + money + "</td>";
+    } else {
+        alert('가격은 숫자만 입력 가능합니다.');
+    }
 }
 
 
 
-function tableDelete()
-{
-    var tableData=document.getElementById('testTable');
+function tableDelete() {
+    var tableData = document.getElementById('testTable');
 
-    if(tableData.rows.length<2)return;
+    if (tableData.rows.length < 2) return;
     tableData.deleteRow(1);
 }
