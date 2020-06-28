19e5a13b3fac76bc9fb1cf15072abae1de58cd65
Jade-00 <shgus2066@gmail.com>
2020-06-28 22:26:32 +0900

    라디오버튼 선택 삭제하기로 수정

--- tableControl.js
+++ tableControl.js
@@ -10,6 +10,8 @@ function tableCreate() {
         var cell2 = row.insertCell(1);
         cell1.innerHTML = "<td>" + ID + "</td>";
         cell2.innerHTML = "<td>" + money + "</td>";
+        var cell3 = row.insertCell(2);
+        cell3.innerHTML="<td><input type='radio' name='radio' width='10px' height='10px'></td>";
     } else {
         alert('가격은 숫자만 입력 가능합니다.');
     }
@@ -17,9 +19,24 @@ function tableCreate() {
 
 
 
-function tableDelete() {
+function tableDelete(){
+    var tableData = document.getElementById('testTable');
+    for(var i = 1;i<tableData.rows.length;i++){
+        var chkbox = tableData.rows[i].cells[2].childNodes[0].checked;
+
+        if(chkbox){
+            tableData.deleteRow(i);
+            i--;
+        }
+    }
+} 
+/*{
     var tableData = document.getElementById('testTable');
 
     if (tableData.rows.length < 2) return;
     tableData.deleteRow(1);
+}*/
+
+function tableRevise(){
+
 }
