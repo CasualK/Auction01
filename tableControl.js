013d71c6c1979886403d6da5527e3440ac8962ff
01ryan <00ryan@kakao.com>
2020-06-29 06:24:59 +0900

    qwer

--- tableControl.js
+++ tableControl.js
@@ -1,46 +1,56 @@
+var present = 0;
+var info = []; //앞뒤 반대 앞1:유저id, 앞2:책이름, 앞3:책가격, 뒤 : 현재 열
+
+
 function tableCreate() {
-    var ID = document.getElementById('inName').value;
+   
+        var name = document.getElementById('inName').value;
     var money = document.getElementById('money').value;
+    var id = document.getElementById('userid').value; 
+    
 
-    if (money >= 0) {
+    if (money >= 0 ) {
+        info[present] = {p_id : id, p_name : name, price : money, time : 1};
         var tableData = document.getElementById('testTable');
-        var row = tableData.insertRow(tableData.rows.length);
-
+        var row = tableData.insertRow(tableData.rows.length); 
         var cell1 = row.insertCell(0);
         var cell2 = row.insertCell(1);
-        cell1.innerHTML = "<td>" + ID + "</td>";
-        cell2.innerHTML = "<td>" + money + "</td>";
-        var cell3 = row.insertCell(2);
-        var cell4 = row.insertCell(3);
-        var cell5 = row.insertCell(4);
-        cell3.innerHTML = "<td></td>";
-        cell4.innerHTML = "<td></td>";
-        cell5.innerHTML="<td><input type='radio' name='radio' width='5px' height='5px'></td>";
+        var cell3 = row.insertCell(2);        
+        cell1.innerHTML = "<td>" + (info[present].p_name) + "</td>";
+        cell2.innerHTML = "<td>" + (info[present].price) + "</td>";
+        cell3.innerHTML = "<td>" + (info[present].time) + "</td>";
+        alert('USERID는'+info[present].p_id+'입니다');
+        present++;
     } else {
         alert('가격은 숫자만 입력 가능합니다.');
     }
+      
 }
- 
-
 
 function tableDelete(){
-    var tableData = document.getElementById('testTable');
-    for(var i = 1;i<tableData.rows.length;i++){
-        var chkbox = tableData.rows[i].cells[4].childNodes[0].checked;
-
-        if(chkbox){
-            tableData.deleteRow(i);
-            i--;
-        }
-    }
+    var id = document.getElementById('userid').value; 
+    var name = document.getElementById('inName').value;
+    var max = present;
+    for(var i = 1 ; i<=present ; i++){
+        if(id == info[i-1].p_id ){
+        var tableData = document.getElementById('testTable');
+        tableData.deleteRow(i);
+        i--;    present--;
+        break;
+        } 
+    } 
 } 
-/*{
-    var tableData = document.getElementById('testTable');
 
-    if (tableData.rows.length < 2) return;
-    tableData.deleteRow(1);
-}*/
+function renew(){ //입찰
+ var p_much = document.getElementById('much').value; //입찰할 가격 
+ var wline = document.getElementById('Aure').value;  //입찰할 행
 
-function tableRevise(){
+ var table1 = document.getElementById("testTable"); //테이블 불러옴
+ var str = table1.rows[wline].cells[1].value;   //이거 수정해야함 기존 데이터 읽어와서 입찰할 가격이랑 비교
+ alert(str);                                    //코드 확인 테스트용 ALERT
+ table1.rows[wline].cells[1].innerHTML = p_much; //입력한 행에 입력한 가격을 넣음
+}
+
+function Revise(){ //수정 만들어야함
 
 }
