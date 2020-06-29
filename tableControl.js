2f9d4646988578289ebf54663529eb9178290701
01ryan <00ryan@kakao.com>
2020-06-29 13:40:22 +0900

    last

--- tableControl.js
+++ tableControl.js
@@ -75,14 +75,13 @@ function end(){ //마감
 
 }
 
-function search() {
- var search = document.getElementById('searchtext').value; 
-    
+function search1() {
+ var newsearch = document.getElementById('searchtext').value; 
  var tableData = document.getElementById('testTable');
 
  for(var i = 1 ; i < 100; i ++ ){
     var indata = tableData.rows[i].cells[0].innerHTML;
-    if(search == indata){
+    if(newsearch == indata){
         tableData.rows[i].cells[0].style.color = "#00AA00";
     }else{
         tableData.rows[i].cells[0].style.color = "#000000";
